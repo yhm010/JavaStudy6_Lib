cde4cba86b933918f7cd22ae7c92cb214af41e5a
yoohaemin <yoohm0630@gmail.com>
2022-01-13 00:13:20 +0900

    a

--- src/com/iu/s3/ex1/MemberMain.java
+++ src/com/iu/s3/ex1/MemberMain.java
@@ -1,7 +1,5 @@
 package com.iu.s3.ex1;
 
-import java.util.ArrayList;
-
 public class MemberMain {
 
 	public static void main(String[] args) {
@@ -28,25 +26,18 @@ public class MemberMain {
 		
 		//test, 프로그램 실행
 		MemberData memberData = new MemberData();
-		ArrayList<MemberDTO> members = memberData.init();
+		MemberDTO [] members = memberData.init();
 		
-		//memberData.addMember(members);
-		MemberDTO memberDTO = memberData.removeMember(members);
-		if(memberDTO != null) {
-			System.out.println("삭제 성공");
-		}else {
-			System.out.println("삭제 실패");
-		}
-		
-		for(int i=0;i<members.size();i++) {
-			System.out.println(members.get(i).getId());
-			System.out.println(members.get(i).getPw());
-			System.out.println(members.get(i).getName());
-			System.out.println(members.get(i).getEmail());
-			System.out.println(members.get(i).getAge());
+		for(int i=0;i<members.length;i++) {
+			System.out.println(members[i].getId());
+			System.out.println(members[i].getPw());
+			System.out.println(members[i].getName());
+			System.out.println(members[i].getEmail());
+			System.out.println(members[i].getAge());
 			System.out.println("================");
 		}
 		
+		System.out.println("name : "+ "iu "+ "age : "+30);
 
 	}
 
